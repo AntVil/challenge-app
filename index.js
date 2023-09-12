1bbbc67fb22211f8d7d9ad78f1856224ea94b5c5
Quentin Risy <quentin.risy@meteoblue.com>
2023-09-12 14:43:58 +0200

    added score counter

--- index.js
+++ index.js
@@ -13,36 +13,37 @@ app.use(express.json());
 
 app.post("/load", (req, res) => {
     let { teamName, teamLocation } = req.body;
-    console.log(teamName, teamLocation);
     if (!(teamName in obj)) {
         obj[teamName] = []
     };
+
+    score = calcScore(teamName)
+    console.log(`hallo`)
+    
     if (!obj[teamName].includes(teamLocation)) {
-        res.send(fs.readFileSync(`challenges/${teamLocation}/${stringToHash(teamName, teamLocation)}.txt`));
+        res.send(score + `\n` + fs.readFileSync(`challenges/${teamLocation}/${stringToHash(teamName, teamLocation)}.txt`));
     }
     else {
-        res.send(`Bitte gebt einen neuen Standort ein`)
+        res.send(score + `\n` + `Bitte gebt einen neuen Standort ein`)
     }
 });
 
 app.post("/complete", (req, res) => {
     let { teamName, teamLocation } = req.body;
-    console.log(teamName, teamLocation);
     if (!obj[teamName].includes(teamLocation)) {
         obj[teamName].push(teamLocation);
     }
-    console.log(obj);
     res.send(`challenge for ${teamName} at ${teamLocation}`);
 });
 
 app.listen(PORT, () => {
-    console.log(challenges)
     console.log(`listening on port ${PORT}`)
 });
 
 function stringToHash(string, teamLocation) {
 
     let hash = 0;
+    string = string +  teamLocation
 
     if (string.length == 0) return hash;
 
@@ -53,4 +54,12 @@ function stringToHash(string, teamLocation) {
 
     hash = hash % fs.readdirSync(`challenges/${teamLocation}`).length;
     return hash;
+}
+
+function calcScore(teamName){
+    score = 0
+    for (let i = 0; i < obj[teamName].length; i++) {
+        score = score + parseInt(obj[teamName][i].slice(1))
+      }
+    return score
 }
