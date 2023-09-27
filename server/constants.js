5fa706c7e855aa3527832e4a23c4b8d070898eeb
Samuel <65895765+Sayum24@users.noreply.github.com>
2023-09-27 21:26:11 +0200

    moved challenges & public folder into server folder

--- server/constants.js
+++ server/constants.js
@@ -5,9 +5,9 @@ import { fileURLToPath } from 'url';
 const CURRENT_DIRECTORY = path.dirname(fileURLToPath(import.meta.url))
 
 export const PORT = 8080;
-export const PUBLIC_DIRECTORY = path.join(CURRENT_DIRECTORY, "..", "public")
-export const CHALLENGE_DIRECTORY = path.join(CURRENT_DIRECTORY, "..", "challenges")
-export const LOG_DIRECTORY = path.join(CURRENT_DIRECTORY, "..", "logs")
+export const PUBLIC_DIRECTORY = path.join(CURRENT_DIRECTORY, "public")
+export const CHALLENGE_DIRECTORY = path.join(CURRENT_DIRECTORY, "challenges")
+export const LOG_DIRECTORY = path.join(CURRENT_DIRECTORY, "logs")
 
 if (!fs.existsSync(LOG_DIRECTORY)){
     fs.mkdirSync(LOG_DIRECTORY);
