18d18fa452fa1920bf5ad19dbf9caf273ee115f2
Samuel <65895765+Sayum24@users.noreply.github.com>
2023-09-29 12:19:53 +0200

    Revert "changed node-fetch"
    This reverts commit 72a75d0bbccb9140b227a5d82e43e090eb931585.

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import cors from "cors";
 import TeamTable from "./TeamTable.js";
 import LocationManager from "./LocationManager.js";
 import { PORT, PUBLIC_DIRECTORY, CHALLENGE_DIRECTORY, LOG_DIRECTORY } from "./constants.js";
-
+import fetch from 'node-fetch';
 
 
 const app = express();
